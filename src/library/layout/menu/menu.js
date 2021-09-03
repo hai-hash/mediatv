2e7754c95af33506068fd5b2592859c17dc0de42
whynotme1131999 <whynotme1131999@gmail.com>
2021-09-03 23:36:35 +0700

    update

--- src/library/layout/menu/menu.js
+++ src/library/layout/menu/menu.js
@@ -1,12 +1,12 @@
 import styles from './../styles.module.scss'
 import {AiFillHome} from 'react-icons/ai'
-import {AiOutlineBlock} from 'react-icons/ai'
-import {BiGlobe} from 'react-icons/bi'
-import {GrMultimedia} from 'react-icons/gr'
-import {RiPlayList2Fill} from 'react-icons/ri'
-import {GiJusticeStar} from 'react-icons/gi'
-import {MdPlaylistPlay} from 'react-icons/md'
-import {BiCaretRightCircle} from 'react-icons/bi'
+// import {AiOutlineBlock} from 'react-icons/ai'
+// import {BiGlobe} from 'react-icons/bi'
+// import {GrMultimedia} from 'react-icons/gr'
+// import {RiPlayList2Fill} from 'react-icons/ri'
+// import {GiJusticeStar} from 'react-icons/gi'
+// import {MdPlaylistPlay} from 'react-icons/md'
+// import {BiCaretRightCircle} from 'react-icons/bi'
 
 const Menu = () =>{
     return (
@@ -14,7 +14,7 @@ const Menu = () =>{
         <div className={styles.menu}>
             <ul className={styles.category}>
                 <li><AiFillHome className={styles.icon_home}/></li>
-                <li><AiOutlineBlock className={styles.icon_category}/><span>Thể loại</span></li>
+                <li><span>Thể loại</span></li>
                 <li><span>Quốc gia</span></li>
                 <li><span>Phim lẻ</span></li>
                 <li><span>Phim bộ</span></li>
