5b20f58b3dbcb98e502efcc254feb902170b71b8
whynotme1131999 <whynotme1131999@gmail.com>
2021-09-06 23:39:52 +0700

    update video

--- src/page/firm/index.js
+++ src/page/firm/index.js
@@ -3,7 +3,7 @@ const Firm = () =>{
     return (
         <div>
              <video  width="100%" height="600" controls autoplay>
-                <source src="http://www.vietjack.com/html5/foo.mp4" type="video/mp4" />
+                <source src="/video/demo1.mp4" type="video/mp4" />
             </video>
 
         </div>
