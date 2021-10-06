4231a8c9ac2ac26b3d53af6fd617ca9151f2bf26
whynotme1131999 <whynotme1131999@gmail.com>
2021-10-06 18:40:20 +0700

    update evaluae

--- src/pageadmin/evaluate/evaluate.js
+++ src/pageadmin/evaluate/evaluate.js
@@ -4,25 +4,25 @@ import styles from './styles.module.scss';
 import * as types from './../../handler/evaluate/evaluateType';
 import EvaluateAdd from './evaluateAdd';
 const Evaluate = () => {
-    const [status, setStatus] = useState(types.DISPLAY);
-    const genData = () => {
-        switch (status) {
-            case types.DISPLAY:
-                return <EvaluateDisplay setStatus={setStatus} />
-            case types.ADD:
-                return <EvaluateAdd setStatus={setStatus} />
-            default:
-                return <EvaluateDisplay />
-        }
-    }
-    const onAdd = () => {
-        if (status === types.DISPLAY)
-            setStatus(types.ADD)
-        else setStatus(types.DISPLAY)
-    }
+    // const [status, setStatus] = useState(types.DISPLAY);
+    // const genData = () => {
+    //     switch (status) {
+    //         case types.DISPLAY:
+    //             return <EvaluateDisplay setStatus={setStatus} />
+    //         case types.ADD:
+    //             return <EvaluateAdd setStatus={setStatus} />
+    //         default:
+    //             return <EvaluateDisplay />
+    //     }
+    // }
+    // const onAdd = () => {
+    //     if (status === types.DISPLAY)
+    //         setStatus(types.ADD)
+    //     else setStatus(types.DISPLAY)
+    // }
     return (
         <div>
-            <div className={styles.url}>
+            {/* <div className={styles.url}>
                 <div className={styles.url_left}>
                     <span>api / admin / Evaluate </span>
                     <h5>Evaluate</h5>
@@ -31,7 +31,7 @@ const Evaluate = () => {
                     <button onClick={onAdd}>Evaluate</button>
                 </div>
             </div>
-            {genData()}
+            {genData()} */}
         </div>
     )
 }
